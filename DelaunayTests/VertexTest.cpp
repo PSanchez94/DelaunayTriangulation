c59ec08d944870f933870e834a9b76ab6357aa3e
PSanchez94 <pablo.sanchez.r@ug.uchile.cl>
2020-04-19 16:58:56 -0400

    Added diff numtypes for vertex test and equals test

--- DelaunayTests/VertexTest.cpp
+++ DelaunayTests/VertexTest.cpp
@@ -7,7 +7,8 @@
 class VertexTest : public::testing::Test {
 protected:
 
-    Vertex<float> testVertex;
+    Vertex<float> testVertexF;
+    Vertex<double> testVertexD;
 
     void SetUp() override {
 
@@ -19,7 +20,33 @@ protected:
 
 };
 
-TEST_F(VertexTest, Constructor) {
-    ASSERT_EQ(testVertex.X, 0);
-    ASSERT_EQ(testVertex.Y, 0);
+TEST_F(VertexTest, FloatConstructor) {
+    ASSERT_EQ(testVertexF.X, 0);
+    ASSERT_EQ(testVertexF.Y, 0);
+}
+
+TEST_F(VertexTest, FloatConstructorParameters) {
+    testVertexF = Vertex<float>(1.1f,2.2f);
+
+    ASSERT_EQ(testVertexF.X, 1.1f);
+    ASSERT_EQ(testVertexF.Y, 2.2f);
+}
+
+TEST_F(VertexTest, DoubleConstructor) {
+    ASSERT_EQ(testVertexD.X, 0);
+    ASSERT_EQ(testVertexD.Y, 0);
+}
+
+TEST_F(VertexTest, DoubleConstructorParameters) {
+    testVertexD = Vertex<double>(1.1f,2.2f);
+
+    ASSERT_EQ(testVertexD.X, 1.1f);
+    ASSERT_EQ(testVertexD.Y, 2.2f);
+}
+
+
+TEST_F(VertexTest,EqualsOperator) {
+    testVertexF = Vertex<float>(1.1f,2.2f);
+    Vertex<float> equalVertex = Vertex<float>(1.1f,2.2f);
+    ASSERT_TRUE(testVertexF == equalVertex);
 }
